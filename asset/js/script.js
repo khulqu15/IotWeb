9f04c1f4bdbb2c22079a1401bcd163260d059ddd
ninno <khusnul20032002@gmail.com>
2020-01-13 21:46:17 -0800

    Initial commit

--- asset/js/script.js
+++ asset/js/script.js
@@ -3,23 +3,40 @@ if( $(window).innerWidth() >= 767 ) {
     $('#menu-close').hide();
 } 
 
-$(window).ready(function(){
+$(document).ready(function(){
+    let name = localStorage.getItem('myNameUser');
+    let username = localStorage.getItem('myUsernameUser');
+    let password = localStorage.getItem('myPasswordUser');
+    let email = localStorage.getItem('myEmailUser');
+    console.log('name ' + name);
+    if(name === undefined && username === undefined && password === undefined && email === undefined ||
+        name === null && username === null && password === null && email === null) {
+            $('#btnLoginUser').show();
+            $('#btnLoginUser').removeClass('d-none');
+            $('#avatarUser').hide();
+            $('#avatarUser').addClass('d-none');
+        } else {
+            $('#btnLoginUser').hide();
+            $('#btnLoginUser').addClass('d-none');
+            $('#avatarUser').show();
+            $('#avatarUser').removeClass('d-none');
+        }
     $('#menu-toggle').click(function(){
+        $('body').addClass('overflowx');
         $('#menu-nav').slideToggle(500);
         $('#menu-toggle').toggle(500);
         $('#menu-close').toggle(500);
         $('#ul-menu li a').addClass('text-black');
-        $('body').addClass('overflowx');
         $('#ul-menu li svg').addClass('text-black');
-    })
+    });
     $('#menu-close').click(function(){
-        $('#menu-nav').hide(500);
+        $('body').removeClass('overflowx');
+        $('#menu-nav').slideToggle(500);
         $('#menu-toggle').toggle(500);
         $('#menu-close').toggle(500);
         $('.settings').hide(500);
         $('#settingsNav').slideToggle(500);
         $('#settingsClose').slideToggle(500);
-        $('body').removeClass('overflowx');
     })
     // $('#settingsNav').click(function() {
     //     $('nav').addClass('nav-down');
@@ -46,17 +63,6 @@ $(window).ready(function(){
     if($(window).innerWidth() <= 767) {
         $('#ul-menu li a').addClass('text-black');
         $('#ul-menu li svg').addClass('text-black');
-        $('#settingsNav').click(function() {
-            $('nav').addClass('nav-down');
-            $('.settings').show(500);
-            $('#ul-menu li a').addClass('text-black');
-            $('#ul-menu li svg').addClass('text-black');    
-            $('#brand-nav p').addClass('text-black');
-            $('.line').addClass('text-white');
-            $('#menu-close').show();
-            $('#menu-nav').slideToggle(500);
-            $('body').removeClass('overflowx');
-        });
     }
 });
 
